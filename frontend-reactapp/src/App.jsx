d6b2528c15b4451c443b9b0e8a99327b06d2a098
SOMESH <someshbiswal2308@gmail.com>
2025-10-18 10:37:04 +0530

    Changed App.jsx

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
